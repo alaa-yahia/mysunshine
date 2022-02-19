43b85833b3479e43b236db0d3f84e0a13e5bc724
Alaa Yahia <alaay873@live.com>
2022-02-19 15:44:43 +0200

    Add header

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header className='flex'>
+      <h1 className='p-3 text-3xl'>Logo</h1>
+      <nav className='flex flex-1 justify-end py-3 px-7'>
+        <ul className='flex space-x-4'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Blog</li>
+          <li>Contact Me</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from '../components/Head';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
     <>
       <Head />
+      <Header />
+      <Hero />
+
       <div className='flex h-screen items-center justify-center'>
         <main>
           <h1 className='text-5xl font-medium leading-tight'>

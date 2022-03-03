3a5e022beee881fe43692ed60bc0d807af38c50a
Alaa Yahia <alaay873@live.com>
2022-03-03 11:01:34 +0200

    add open graph & twitter meta tags

--- components/Head.js
+++ components/Head.js
@@ -1,21 +1,32 @@
 import NextHead from 'next/head';
 
-const Head = ({ title, keywords, description }) => {
+const Head = ({ title, keywords, description, url, ogType, img }) => {
   return (
     <NextHead>
       <title>{title}</title>
       <meta name='keywords' content={keywords} />
       <meta name='description' content={description} />
+      <meta property='og:title' content={title} />
+      <meta property='og:url' content={url} />
+      <meta property='og:description' content={description} />
+      <meta property='og:type' content={ogType} />
+      <meta property='og:image' content={img} />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:site' content='@agribeenia' />
+      <meta name='twitter:creator' content='@agribeenia' />
       <link rel='icon' href='/favicon.ico' />
     </NextHead>
   );
 };
 
 Head.defaultProps = {
-  title: 'Alaa portfolio and blog',
+  title: 'Alaa portfolio and blog', //title of the page
   keywords:
     'portfolio, blog, web development, nextjs, react, frontend, html, css',
   description: 'Alaa portfolio and blog about web development',
+  ogType: 'profile', //article
+  url: 'Alla.sd', //url of the page
+  img: '', //The URL of the image that appears when someone shares the content to social media
 };
 
 export default Head;
